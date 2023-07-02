249a44925a2baa273ae2705ae97b0e51bfd1c4f3
Rojan Maharjan <rojanmhj@gmail.com>
2023-07-02 01:01:49 -0700

    added 12. romanToInt problem and solution, file restructing

---
+++ Top Interview 150/Array Problems/README.md
@@ -0,0 +1,8 @@
+# Tips for working with array problems
+
+1. Array problems have a simple brute force solution that use O(n) space, but solutions that use same array to reduce space complexity to O(1).
+2. Try filling the **array from the back.**
+3. Instead deleting an entry, consider **overwriting** it.
+4. Be comfortable with writing code that operates on **subarrays**.
+5. **off by 1** is easy error to make with catastrophic consequences.
+6. Don't worry about integretiy of array (sort, keeping equal entries together), until it is time to return.
---
+++ Top Interview 150/String Problems/Roman To Integer/romanToInt.py
@@ -0,0 +1,26 @@
+#Problem Statement: Given a Roman string return its 
+#Integer value
+
+# url : https://leetcode.com/problems/roman-to-integer/?envType=study-plan-v2&envId=top-interview-150
+
+def romanToInt(s:str)->int:
+    roman = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+    value = roman[s[-1]]
+    for i in reversed(range(len(s)-1)):
+        if roman[s[i]]<roman[s[i+1]]:
+            value = value - roman[s[i]]
+        else:
+            value = value + roman[s[i]] 
+    
+    return value
+
+## solution using Functool
+
+import functools
+
+def romanToInt(self, s: str) -> int:
+    roman = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+    
+    return functools.reduce(
+    lambda val,i: val+ (-roman[s[i]] if roman[s[i]] < roman [s[i+1]] else roman[s[i]]),reversed(range(len(s)-1)),roman[s[-1]])
+    
